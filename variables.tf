475d7c019aa2d3fef103000ff502b1937fcc2b4e
BukolaAkande <90542569+BukolaAkande@users.noreply.github.com>
2021-11-09 22:32:51 -0600

    Update variables.tf
    apply change

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins4-security-group"
+  default     = "my-jenkins10-security-group"
 }
 
 variable "tag_name" {
